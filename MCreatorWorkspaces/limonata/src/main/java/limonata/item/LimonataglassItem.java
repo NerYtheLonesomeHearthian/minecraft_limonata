b5188ef2c876edcdb3c9ed752812cdcf797055d4
nery <130411048+NerYtheLonesomeHearthian@users.noreply.github.com>
2025-07-26 22:35:47 +0200

    commit_after-v1.2.0.3_260725_2235

--- MCreatorWorkspaces/limonata/src/main/java/limonata/item/LimonataglassItem.java
+++ MCreatorWorkspaces/limonata/src/main/java/limonata/item/LimonataglassItem.java
@@ -1,14 +1,23 @@
 package limonata.item;
 
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.api.distmarker.Dist;
+
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.network.chat.Component;
+
+import limonata.procedures.LimonataglassPlayerFinishesUsingItemProcedure;
 
 import limonata.init.LimonataModItems;
+
+import java.util.List;
 
 public class LimonataglassItem extends Item {
 	public LimonataglassItem() {
@@ -26,9 +35,20 @@ public class LimonataglassItem extends Item {
 	}
 
 	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void appendHoverText(ItemStack itemstack, Item.TooltipContext context, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, context, list, flag);
+		list.add(Component.translatable("item.limonata.limonataglass.description_0"));
+	}
+
+	@Override
 	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
 		ItemStack retval = new ItemStack(LimonataModItems.GLASS.get());
 		super.finishUsingItem(itemstack, world, entity);
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		LimonataglassPlayerFinishesUsingItemProcedure.execute(entity);
 		if (itemstack.isEmpty()) {
 			return retval;
 		} else {
