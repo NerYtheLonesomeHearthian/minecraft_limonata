35e5e9c6e4fa8577c0c522cffd2a04b343f50a29
nery <130411048+NerYtheLonesomeHearthian@users.noreply.github.com>
2025-08-16 17:16:49 +0200

    1.3.2 completed (creative tab)

--- MCreatorWorkspaces/limonata/src/main/java/limonata/init/LimonataModTabs.java
+++ MCreatorWorkspaces/limonata/src/main/java/limonata/init/LimonataModTabs.java
@@ -4,12 +4,15 @@
 package limonata.init;
 
 import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.Registries;
 
 import limonata.LimonataMod;
@@ -17,24 +20,38 @@ import limonata.LimonataMod;
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 public class LimonataModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, LimonataMod.MODID);
+	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> LIMONATA_CREATIVE_TAB = REGISTRY.register("limonata_creative_tab",
+			() -> CreativeModeTab.builder().title(Component.translatable("item_group.limonata.limonata_creative_tab")).icon(() -> new ItemStack(LimonataModItems.LEMONADE_ACHIEVEMENT_ITEM.get())).displayItems((parameters, tabData) -> {
+				tabData.accept(LimonataModItems.MUSIC_DISC_LIMONATA_CHIAMATA.get());
+				tabData.accept(LimonataModBlocks.LEMONADE_STAND.get().asItem());
+				tabData.accept(LimonataModItems.GLASS.get());
+				tabData.accept(LimonataModItems.GLASS_OF_LEMONADE.get());
+				tabData.accept(LimonataModBlocks.LEMON_CRATE.get().asItem());
+				tabData.accept(LimonataModItems.LEMON.get());
+				tabData.accept(LimonataModItems.CUT_LEMON.get());
+				tabData.accept(LimonataModItems.LEMON_SEEDS.get());
+				tabData.accept(LimonataModBlocks.LEMON_LEAVES.get().asItem());
+				tabData.accept(LimonataModBlocks.LEMON_LEAVES_LEMON.get().asItem());
+				tabData.accept(LimonataModItems.ICE_CUBES.get());
+			}).build());
 
 	@SubscribeEvent
 	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
-		if (tabData.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
-			tabData.accept(LimonataModItems.LEMON_SEEDS.get());
-			tabData.accept(LimonataModBlocks.LEMON_LEAVES.get().asItem());
-			tabData.accept(LimonataModBlocks.LEMON_LEAVES_LEMON.get().asItem());
-		} else if (tabData.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
+		if (tabData.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
 			tabData.accept(LimonataModBlocks.LEMONADE_STAND.get().asItem());
 			tabData.accept(LimonataModBlocks.LEMON_CRATE.get().asItem());
 		} else if (tabData.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
 			tabData.accept(LimonataModItems.MUSIC_DISC_LIMONATA_CHIAMATA.get());
 			tabData.accept(LimonataModItems.GLASS.get());
 		} else if (tabData.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
-			tabData.accept(LimonataModItems.LEMON.get());
-			tabData.accept(LimonataModItems.ICE_CUBES.get());
 			tabData.accept(LimonataModItems.GLASS_OF_LEMONADE.get());
+			tabData.accept(LimonataModItems.LEMON.get());
 			tabData.accept(LimonataModItems.CUT_LEMON.get());
+			tabData.accept(LimonataModItems.ICE_CUBES.get());
+		} else if (tabData.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
+			tabData.accept(LimonataModBlocks.LEMON_LEAVES.get().asItem());
+			tabData.accept(LimonataModBlocks.LEMON_LEAVES_LEMON.get().asItem());
+			tabData.accept(LimonataModItems.LEMON_SEEDS.get());
 		}
 	}
 }
